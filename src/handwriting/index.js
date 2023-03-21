55b4c46db0c2b86ef640e1e6fa48123c45997295
xuzhenlei <xuzhenlei@mafengwo.com>
2023-03-22 00:14:35 +0800

    feat: customFilter

--- src/handwriting/index.js
+++ src/handwriting/index.js
@@ -1,19 +1,19 @@
 // 1. 数组打平
 function flatArrWithFlat(arr) {
-  return arr.flat(Infinity);
+    return arr.flat(Infinity);
 }
 
 function flatArrWithReduce(arr) {
-  const resultArr = arr.reduce((prev, cur) => {
-    return prev.concat(Array.isArray(cur) ? flatArrWithReduce(cur) : cur);
-  }, []);
-  return resultArr;
+    const resultArr = arr.reduce((prev, cur) => {
+        return prev.concat(Array.isArray(cur) ? flatArrWithReduce(cur) : cur);
+    }, []);
+    return resultArr;
 }
 
 // 2. 数组去重
 
 function uniqueArrWithSet(arr) {
-  return Array.from(new Set(arr));
+    return Array.from(new Set(arr));
 }
 
 function uniqueArrWithSplice(params) {}
@@ -22,17 +22,47 @@ function uniqueArrWithIndexOf(params) {}
 
 function uniqueArrWithInclude(params) {}
 
-function uniqueArrWithFilter(params) {}
+function uniqueArrWithFilter(arr) {
+    return arr.filter((item, index) => {
+        return arr.indexOf(item) === index;
+    });
+}
 
 function uniqueArrWithMap(params) {}
 
+// 3. 类数组转化为数组
+
+// 4. Array.prototype.filter()
+
+function customFilter(callback, thisArg) {
+    if (!this) {
+        throw new TypeError('this is null or not undefined');
+    }
+
+    if (typeof callback !== 'function') {
+        throw new TypeError(callback + 'is not a function');
+    }
+
+    const res = [];
+    const length = this?.length >>> 0;
+
+    for (let i = 0; i < length; i++) {
+        if (callback.call(thisArg, this[i], i, this)) {
+            res.push(this[i]);
+        }
+    }
+
+    return res;
+}
+
 export {
-  flatArrWithFlat,
-  flatArrWithReduce,
-  uniqueArrWithSet,
-  uniqueArrWithSplice,
-  uniqueArrWithMap,
-  uniqueArrWithIndexOf,
-  uniqueArrWithFilter,
-  uniqueArrWithInclude,
+    flatArrWithFlat,
+    flatArrWithReduce,
+    uniqueArrWithSet,
+    uniqueArrWithSplice,
+    uniqueArrWithMap,
+    uniqueArrWithIndexOf,
+    uniqueArrWithFilter,
+    uniqueArrWithInclude,
+    customFilter,
 };
--- src/pages/index/index.jsx
+++ src/pages/index/index.jsx
@@ -5,10 +5,19 @@ import {
     flatArrWithFlat,
     flatArrWithReduce,
     uniqueArrWithSet,
+    uniqueArrWithFilter,
+    customFilter,
 } from '../../handwriting/index';
 
 const IndexPage = () => {
-    console.log(testUniqueArr, '->', uniqueArrWithSet(testUniqueArr));
+    console.log(testUniqueArr, '->', uniqueArrWithFilter(testUniqueArr));
+
+    const sourceArr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    sourceArr.customFilter = customFilter;
+    const result = sourceArr.customFilter((item) => {
+        return item > 5;
+    });
+    console.log(result, 'filter result');
     return (
         <div>
             <Card>
